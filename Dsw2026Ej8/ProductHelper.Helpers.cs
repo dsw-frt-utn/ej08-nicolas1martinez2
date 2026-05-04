bf3f0d8f92ad7d1e3a4e7cf860c6c13eaa6b8044
Eduardo Martinez <nicoeduardo87.9@gmail.com>
2026-05-04 19:52:02 -0300

    Problema 1

---
+++ Dsw2026Ej8/ProductHelper.Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        private const string FormatoMoneda = "C2";
+
+        private string FormatearPrecio(decimal price)
+        {
+            return price.ToString(FormatoMoneda);
+        }
+    }
+
+}
---
+++ Dsw2026Ej8/ProductHelper.Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            string precioFormateado = FormatearPrecio(price);
+            return $"[{code}] {description} - {precioFormateado}";
+        }
+    }
+
+}
