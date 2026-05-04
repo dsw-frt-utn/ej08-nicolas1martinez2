d3054961e13ac047217587a5cab51066fe3bb859
Eduardo Martinez <nicoeduardo87.9@gmail.com>
2026-05-04 20:02:53 -0300

    Problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            
+            int copia = originalValue;
+            copia++;
+
+            
+            Product productoCopia = product;
+            productoCopia.ModificarDescripcion("Descripcion modificada");
+
+            return $"{originalValue}-{copia}-{product.Description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string Description { get; private set; }
+
+        public Product(string description)
+        {
+            Description = description;
+        }
+
+        public void ModificarDescripcion(string nuevaDescripcion)
+        {
+            Description = nuevaDescripcion;
+        }
+    }
+}
