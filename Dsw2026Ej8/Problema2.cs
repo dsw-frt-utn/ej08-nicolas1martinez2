3a924dde8639d9d4e36a989c45dc28510c20c458
Eduardo Martinez <nicoeduardo87.9@gmail.com>
2026-05-04 19:57:17 -0300

    Problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total = quantity > 0 ? quantity * unitPrice : 0;
+
+            var venta = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = total
+            };
+
+            return $"{venta.Code}-{venta.Description}-{venta.Total}";
+        }
+    }
+
+}
