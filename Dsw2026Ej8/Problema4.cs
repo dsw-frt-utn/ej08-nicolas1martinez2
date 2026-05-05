7949bbabafa4265ca377d091188e5bcc2688f47f
Eduardo Martinez <nicoeduardo87.9@gmail.com>
2026-05-04 23:09:44 -0300

    Problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            var notas = new[] { nota1, nota2, nota3 };
+
+            var notasValidas = notas
+                .Where(n => n.HasValue && n.Value >= 0 && n.Value <= 10)
+                .Select(n => n.Value)
+                .ToList();
+
+            if (notasValidas.Count == 0)
+                return 0;
+
+            return notasValidas.Average();
+        }
+    }
+
+
+}
