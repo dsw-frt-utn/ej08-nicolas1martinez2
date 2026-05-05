6d749cb949e52a04f5fa915742c41f4ee8a7a59e
Eduardo Martinez <nicoeduardo87.9@gmail.com>
2026-05-04 23:24:49 -0300

    Problema 6

---
+++ Dsw2026Ej8/Problema6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            return code.ToProductCode();
+        }
+    }
+
+}
